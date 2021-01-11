2058d51bdece9b668faa1f28c7b3b12d0cce2cec
ozyilmazlar <ogzylmzlr26@yahoo.com>
2021-01-11 10:25:54 +0300

    Added API example

---
+++ example/WordGameAPIExample.java
@@ -0,0 +1,38 @@
+import me.acablade.kelimeoyunu.KelimeOyunu;
+import me.acablade.kelimeoyunu.Objects.WordCheckers.IWordChecker;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordGameAPIExample extends JavaPlugin {
+
+    IWordChecker wordChecker;
+
+    @Override
+    public void onEnable() {
+        //You add it to manager like this (returns IWordChecker so if you want to do anything in the future you can)
+        wordChecker = KelimeOyunu.wordCheckerManager.register(new ExampleWordChecker());
+    }
+
+
+    //Create the word checker class here (you can create independent class aswell, for showing purposes i used inner class)
+    private static class ExampleWordChecker implements IWordChecker {
+        List<String> wordList;
+        public ExampleWordChecker(){
+            this.wordList = new ArrayList<>();
+            wordList.add("xd");
+            wordList.add("lol");
+            wordList.add("bruh");
+        }
+        @Override
+        public boolean isWord(String s) {
+            return wordList.contains(s);
+        }
+
+        @Override
+        public String[] getLanguage() {
+            return new String[]{"Example"};
+        }
+    }
+}
